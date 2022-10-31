33efda2fe207723b6e72f8a95a353cf8b6e314cc
obkin <kapellwork@gmail.com>
2022-10-31 16:00:33 +0200

    first-animation

--- script.js
+++ script.js
@@ -3,3 +3,13 @@
 const btnPhone = document.querySelector('#iphone'),
       btnMacbook = document.querySelector('#macbook'),
       images = document.querySelectorAll('img');
+
+const phoneAnimation = images[0].animate([
+    {transform: 'translateY(0)'},
+    {transform: 'translateY(100px)'},
+    {transform: 'translateY(-100px)'},
+    {transform: 'translateY(0)'}
+], {
+    duration: 3000,
+    iterations: Infinity
+});
