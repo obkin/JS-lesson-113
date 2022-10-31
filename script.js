1217d8c1b6f0176a6637318df11383121bdc9801
obkin <kapellwork@gmail.com>
2022-10-31 17:22:41 +0200

    second-animation_with_btn

--- script.js
+++ script.js
@@ -4,12 +4,22 @@ const btnPhone = document.querySelector('#iphone'),
       btnMacbook = document.querySelector('#macbook'),
       images = document.querySelectorAll('img');
 
-const phoneAnimation = images[0].animate([
-    {transform: 'translateY(0)'},
-    {transform: 'translateY(100px)'},
-    {transform: 'translateY(-100px)'},
-    {transform: 'translateY(0)'}
-], {
-    duration: 3000,
-    iterations: Infinity
+let phoneAnimation;
+
+btnPhone.addEventListener('click', () => {
+    if (!phoneAnimation) {
+        phoneAnimation = images[0].animate([
+            {transform: 'translateY(0)'},
+            {transform: 'translateY(100px)'},
+            {transform: 'translateY(-100px)'},
+            {transform: 'translateY(0)'}
+        ], {
+            duration: 3000,
+            iterations: Infinity
+        });
+    } else if (phoneAnimation.playState === 'paused') {
+        phoneAnimation.play();
+    } else {
+        phoneAnimation.pause();
+    }
 });
